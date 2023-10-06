7f42dd32a7ed69c08fdbee4589e7cbabe89b0dce
Ella Hagen <elhagen@calpoly.edu>
2023-10-05 22:02:31 -0700

    connecting frontend and backend

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,5 +1,5 @@
 // src/MyApp.js
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Table from "./Table";
 import Form from './Form';
 
@@ -7,6 +7,12 @@ import Form from './Form';
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, [] );
 
   function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
@@ -15,11 +21,34 @@ function MyApp() {
     setCharacters(updated);
   }
 
-
   function updateList(person) {
     setCharacters([...characters, person]);
   }
-  
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 
   return (
     <div className="container">
